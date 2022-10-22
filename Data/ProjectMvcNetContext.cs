92739e8c502f5cbbaa685714a158541240c94be8
Joao vitor <victor.flop@hotmail.com>
2022-10-22 19:24:28 -0300

    Adicionando departments via Scaffolding

---
+++ Data/ProjectMvcNetContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectMvcNet.Models;
+
+namespace ProjectMvcNet.Data
+{
+    public class ProjectMvcNetContext : DbContext
+    {
+        public ProjectMvcNetContext (DbContextOptions<ProjectMvcNetContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectMvcNet.Models.Department> Department { get; set; }
+    }
+}
