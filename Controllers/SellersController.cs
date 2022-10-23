11581b713f5fee523157416a92077e48be87db6f
Joao vitor <victor.flop@hotmail.com>
2022-10-23 13:48:10 -0300

    Sellers controller

---
+++ Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectMvcNet.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
