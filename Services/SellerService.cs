a4fa00d6eede8746290b3a3bba53343a319349c8
Joao vitor <victor.flop@hotmail.com>
2022-10-29 13:46:26 -0300

    .

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -19,6 +19,18 @@ namespace ProjectMvcNet.Services
             return _context.Seller.ToList();
         }
 
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(s => s.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+
         public void Insert(Seller seller)
         {
             _context.Add(seller);
